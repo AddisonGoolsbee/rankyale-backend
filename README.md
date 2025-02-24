64e5df6c515d652ae26121abe44d457bb7703dd9
Addison Goolsbee <addisongoolsbee@gmail.com>
2025-02-24 14:50:42 -0500

    Add functioning elo function

--- README.md
+++ README.md
@@ -4,4 +4,18 @@ This is the Firebase Cloud Functions for rankyale.com: a toxic website where Yal
 
 ## Development
 
+`npm run build` and then `firebase emulators:start` to run in dev
 `firebase deploy --only functions` to deploy to firebase
+
+```bash
+curl -X POST "http://127.0.0.1:5001/rankyale/us-central1/updateEloRating" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "data": {
+      "collectionName": "colleges",
+      "entry1Id": "a",
+      "entry2Id": "b",
+      "mode": 0
+    }
+  }'
+```
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,20 +1,67 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+// import {onCall} from "firebase-functions/v2/https";
+// import * as admin from "firebase-admin";
+// import * as logger from "firebase-functions/logger";
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+// admin.initializeApp();
+// const db = admin.firestore();
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+// The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
+import {logger} from "firebase-functions";
+import {onCall, onRequest} from "firebase-functions/v2/https";
+// import {onDocumentCreated} from "firebase-functions/v2/firestore";
 
+// The Firebase Admin SDK to access Firestore.
+import {initializeApp} from "firebase-admin/app";
+import {getFirestore} from "firebase-admin/firestore";
 
-export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+initializeApp();
+const db = getFirestore();
+
+const K = 32; // Elo rating K-factor
+
+export const updateEloRating = onCall(async (request) => {
+  logger.info("Received updateEloRating request", request.data);
+
+  const {collectionName, entry1Id, entry2Id, mode} = request.data;
+
+  if (!collectionName || !entry1Id || !entry2Id) {
+    throw new Error("Invalid request: Missing parameters");
+  }
+
+  const entry1Ref = db.collection("categories").doc(collectionName).collection("entries").doc(entry1Id);
+  const entry2Ref = db.collection("categories").doc(collectionName).collection("entries").doc(entry2Id);
+
+  const [entry1Doc, entry2Doc] = await Promise.all([entry1Ref.get(), entry2Ref.get()]);
+
+  if (!entry1Doc.exists || !entry2Doc.exists) {
+    throw new Error("One or both entries not found");
+  }
+
+  const entry1 = entry1Doc.data();
+  const entry2 = entry2Doc.data();
+
+  if (entry1 === undefined || entry2 === undefined) {
+    throw new Error("One or both entries do not have a score");
+  }
+
+  const expectedScore1 = 1 / (1 + Math.pow(10, (entry2.score - entry1.score) / 400));
+  const expectedScore2 = 1 / (1 + Math.pow(10, (entry1.score - entry2.score) / 400));
+
+  let score1 = entry1.score;
+  let score2 = entry2.score;
+
+  if (mode === 0) {
+    score1 += K * (1 - expectedScore1);
+    score2 += K * (0 - expectedScore2);
+  } else if (mode === 1) {
+    score1 += K * (0 - expectedScore1);
+    score2 += K * (1 - expectedScore2);
+  }
+
+  await Promise.all([
+    entry1Ref.update({score: score1}),
+    entry2Ref.update({score: score2}),
+  ]);
+
+  return {message: "Elo scores updated", entry1Id, newScore1: score1, entry2Id, newScore2: score2};
 });
